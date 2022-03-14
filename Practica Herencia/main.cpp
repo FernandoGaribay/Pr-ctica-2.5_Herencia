e4eebd3a97103e93c54e501941435557bbca2127
Fernando Garibay <garibaycejaf@gmail.com>
2022-03-14 12:30:38 -0600

    + Prueba de la funcion calcularIntereses();

--- Practica Herencia/main.cpp
+++ Practica Herencia/main.cpp
@@ -11,6 +11,9 @@ int main()
     Ahorros objAhorros("Fernando", 21310414, 100, 10, 5);
     Cheques objCheques("Ivan", 21310418, 200, 10);
 
+    cout << "objAhorros : calcularIntereses(); = ";
+    objAhorros.calcularIntereses();
+    cout << objAhorros.getSaldo();
 
     return 0;
 }
